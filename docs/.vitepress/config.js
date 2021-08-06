c101d7e76d245ad0b1f656c74790c987ef266466
SmokeyStack <84206875+SmokeyStack@users.noreply.github.com>
2021-08-06 18:11:44 -0400

    Added Add-On section and updated AboutMe

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -2,69 +2,12 @@ module.exports = {
 	lang: 'en-US',
 	title: 'SmokeyStack',
 	description: 'Website for SmokeyStack.',
-
-	head: [
-		// Enable to make the bedrock wiki installable
-		// [
-		// 	'link',
-		// 	{
-		// 		rel: 'manifest',
-		// 		type: 'application/json',
-		// 		href: '/manifest.webmanifest',
-		// 	},
-		// ],
-		// [
-		// 	'script',
-		// 	{
-		// 		src: '/registerSW.js',
-		// 	},
-		// ],
-	],
-
 	markdown: {
 		lineNumbers: true
 	},
-
 	themeConfig: {
-		//repo: 'bedrock-oss/bedrock-wiki-vite',
 		docsDir: 'docs',
-
 		lastUpdated: 'Last Updated',
-		// vitepress config right now
-		/*algolia: {
-			apiKey: 'c57105e511faa5558547599f120ceeba',
-			indexName: 'vitepress',
-		},
-
-		editLinks: true,
-		editLinkText: 'Edit this page on GitHub',
-
-		nav: [
-			{
-				text: 'Discord',
-				link: '/discord',
-				activeMatch: '^/discord',
-			},
-			{
-				text: 'Contribute',
-				link: '/contribute',
-				activeMatch: '^/contribute',
-			},
-			{
-				text: 'Docs',
-				link: 'https://bedrock.dev',
-			},
-			{
-				text: 'MS Docs',
-				link: 'https://docs.microsoft.com/en-us/minecraft/creator/',
-			},
-			// {
-			// 	text: 'News',
-			// 	link: '/news',
-			// 	activeMatch: '^/news',
-			// },
-		],*/
-
 		sidebar: [
 			{
 				text: 'About',
