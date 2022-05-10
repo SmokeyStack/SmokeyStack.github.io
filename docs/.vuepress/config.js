95d00c6bab0a40ac70e2b730bfdcc05d180f167b
SmokeyStack <84206875+SmokeyStack@users.noreply.github.com>
2022-05-10 12:13:59 -0400

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,25 +1,26 @@
-const { path } = require('@vuepress/utils');
+const { path } = require("@vuepress/utils");
 
 module.exports = {
-	lang: 'en-US',
-	title: 'SmokeyStack',
-	description: 'This is my website',
-	theme: path.resolve(__dirname, './theme/local-theme/index.js'),
-	themeConfig: {
-		contributors: false,
-		darkMode: false,
-		sidebar: {
-			'/': [
-				{
-					text: 'About',
-					collapsible: true,
-					children: ['/about/about_me.md', '/about/links.md'],
-				},
-				{
-					text: 'Downloads',
-					link: '/downloads/coming_soon.md'
-				},
-			],
-		},
-	},
-}
+    lang: "en-US",
+    title: "SmokeyStack",
+    description: "This is my website",
+    theme: path.resolve(__dirname, "./theme/local-theme/index.js"),
+    themeConfig: {
+        contributors: false,
+        darkMode: false,
+        sidebar: {
+            "/": [
+                {
+                    text: "About",
+                    collapsible: true,
+                    children: ["/about/about_me.md", "/about/links.md"],
+                },
+                {
+                    text: "Downloads",
+                    collapsible: true,
+                    children: ["/downloads/add_on.md", "/downloads/program.md"],
+                },
+            ],
+        },
+    },
+};
