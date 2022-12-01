ab51c219e561e939c0eafed41531f4cb86a4e077
SmokeyStack <seansyahril04@gmail.com>
2022-12-01 08:58:09 -0500

    404...

--- docs/.vuepress/theme/local-theme/layouts/404.vue
+++ docs/.vuepress/theme/local-theme/layouts/404.vue
@@ -1,20 +1,29 @@
 <template>
   <div style="margin: auto; width: 60%; padding: 10px">
     <h1>404</h1>
-    <img :src="getImg" width="1080" />
-    <blockquote>{{ getMsg }}</blockquote>
+    <img :src="selectedImage" width="100%" />
+    <blockquote>{{ selectedMessage }}</blockquote>
     <a :href="$site.base" aria-label="go to home"> Respawn </a>
   </div>
 </template>
 
-<script setup lang="ts">
-import { defineComponent } from "vue";
-const msgs = [
-  `From the highest peak to the deepest depths, we've searched far and wide and still can't find the page you're looking for!`,
-  `From the hellish nether to the barren end, we've searched far and wide and still can't find the page you're looking for!`,
-];
-const imgs = ["/assets/images/dark.png", "/assets/images/nether.png"];
-const chance = Math.floor(Math.random() * msgs.length);
-const getImg = imgs[chance];
-const getMsg = msgs[chance];
+<script>
+export default {
+  data() {
+    return {
+      images: ["/assets/images/dark.png", "/assets/images/nether.png"],
+      messages: [
+        "From the highest peak to the deepest depths, we've searched far and wide and still can't find the page you're looking for!",
+        "From the hellish nether to the barren end, we've searched far and wide and still can't find the page you're looking for!",
+      ],
+      selectedImage: "",
+      selectedMessage: "",
+    };
+  },
+  created() {
+    const idx = Math.floor(Math.random() * this.images.length);
+    this.selectedImage = this.images[idx];
+    this.selectedMessage = this.messages[idx];
+  },
+};
 </script>
